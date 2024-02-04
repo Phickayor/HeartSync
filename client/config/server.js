d73a742abdfd6efffffd38dcec3470c3fc4caa57
Phickayor <jetawof@gmail.com>
2024-02-04 02:51:20 +0100

    Fixing typo

--- client/config/server.js
+++ client/config/server.js
@@ -1,3 +1,6 @@
-const baseUrl = process.env.NEXT_PUBLIC_ENIROMENT =="production"? process.env.NEXT_PUBLIC_LIVE_URL:process.env.NEXT_PUBLIC_LOCAL_URL;
+const baseUrl =
+  process.env.NEXT_PUBLIC_ENVIRONMENT == "production"
+    ? process.env.NEXT_PUBLIC_LIVE_URL
+    : process.env.NEXT_PUBLIC_LOCAL_URL;
 
 module.exports = baseUrl;
