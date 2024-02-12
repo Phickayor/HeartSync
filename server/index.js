4add6cb477fb0304cbc2ad21b35ba9ed249f7cc5
Phickayor <jetawof@gmail.com>
2024-02-12 19:51:30 +0100

    Incement of payload storage

--- server/index.js
+++ server/index.js
@@ -10,8 +10,9 @@ const chat_route = require("./src/routes/preference.route");
 const connectToDb = require("./src/config/db.config");
 const bodyParser = require("body-parser");
 connectToDb();
-
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json());
 app.use(cors());
 app.get("/", (req, res) => {
   res.send("Welcome to the backend of BabyBoo");
