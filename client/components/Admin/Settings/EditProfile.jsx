e8eb1e9b3d861e83a21fb39c0d51cdfe2ae17b6f
Phickayor <jetawof@gmail.com>
2024-04-12 19:36:42 +0100

    Edit profile ui update

--- client/components/Admin/Settings/EditProfile.jsx
+++ client/components/Admin/Settings/EditProfile.jsx
@@ -5,18 +5,20 @@ function EditProfile({ onClose, name, keyName, onSubmit }) {
   const [value, setValue] = useState("");
   return (
     <div
-      className={`fixed top-0 left-0  h-screen z-20 w-screen flex flex-col justify-center`}
+      className={`fixed top-0 left-0  bg-opacity-20 bg-black h-screen z-20 w-screen flex flex-col justify-center`}
     >
-      <div className="p-6 py-8 rounded-xl bg-[#262626] border border-white text-white mx-auto w-2/5">
+      <div className="p-6 py-8 rounded-xl border border-white bg-white mx-auto w-10/12 md:w-2/5">
         <div className="relative flex justify-center">
-          <h1 className="text-center text-2xl self-center">Edit {name}</h1>
+          <h1 className="text-center md:text-2xl text-lg self-center">
+            Edit {name}
+          </h1>
           <AiFillCloseCircle
             className="absolute right-6 text-3xl self-center cursor-pointer hover:scale-110"
             onClick={onClose}
           />
         </div>
         <form
-          className="flex flex-col gap-5 pt-10"
+          className="flex flex-col gap-5 py-5 md:pt-10"
           onSubmit={(e) => onSubmit(e, keyName, value)}
         >
           <input
@@ -32,13 +34,13 @@ function EditProfile({ onClose, name, keyName, onSubmit }) {
             onChange={(e) => {
               setValue(e.target.value);
             }}
-            className="py-5 px-6 rounded-lg bg-[#1E1D1D] focus:outline-none"
+            className="md:py-5 py-4 px-6 rounded-lg bg-[#1E1D1D] text-center md:text-start text-white focus:outline-none"
           />
           <button
             type="submit"
-            className="bg-[#584296] text-white md:px-20 rounded-lg py-3 md:text-xl md:self-center mt-4"
+            className="bg-[#584296]  text-white px-16 md:px-20 rounded-lg py-3 md:text-xl self-center md:mt-4"
           >
-            Save
+            Next
           </button>
         </form>
       </div>
