9c5310145731f8b64c3b65febf07932c6ac76a12
Olufikayomi Jetawo <71492979+Phickayor@users.noreply.github.com>
2024-08-14 07:47:10 +0100

    cluster update leading to change in url

--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const connectToDb = async () => {
   try {
     let db = await mongoose.connect(
-      `mongodb+srv://webcraft:${process.env.DB_PSWD}@cluster0.i4m6fkx.mongodb.net/db`
+      `mongodb+srv://webcraft:${process.env.DB_PSWD}@cluster0.tnfikyc.mongodb.net/db`
     );
     console.log("Connected to db successfully at " + db.connection.host);
   } catch (error) {
